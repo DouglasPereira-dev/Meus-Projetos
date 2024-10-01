1b5ea65acdc7b7eecd1ef9a7c61dc3191d410b3e
DouglasPereira-dev <douglaspsilva11@gmail.com>
2024-10-01 19:47:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-document.querySelectorAll('.art-item').forEach(item => {
-    item.addEventListener('click', () => {
-        item.classList.toggle('clicked');
+// Seleciona todas as perguntas
+const faqQuestions = document.querySelectorAll('.faq-question');
+
+// Adiciona um evento de clique a cada pergunta
+faqQuestions.forEach(question => {
+    question.addEventListener('click', () => {
+        // Alterna a exibição da resposta correspondente
+        const answer = question.nextElementSibling; // Seleciona a resposta que está logo após a pergunta
+        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
     });
 });
